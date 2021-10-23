19bf964e5646c536b4a4c325165153cc94dfacee
JanaZhakPl <janazhakpl@gmail.com>
2021-10-23 10:38:46 +0200

    Task complete.

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         int toothCounter = 32;
-
-        //напишите тут ваш код
-
+        toothCounter -= 9;
 
         System.out.println(toothCounter);
     }
